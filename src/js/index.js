39e372d2022ee4b891cba3124a0dd95921c1a3a9
Yasmim Coimbra <ymdsc.coimbra85@gmail.com>
2025-02-13 00:53:19 -0300

    JS: adding functionality: if all required fields are filled, it sets the button's attribute type to "submit"; if not, it sets it to "button"

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,41 @@
 camposObrigatorios = document.querySelectorAll(".campo-obrigatorio");
+btnEnviar = document.querySelector("button");
 mensagemErro = document.querySelectorAll(".erro");
+
+btnEnviar.addEventListener("click", function validarComBotao() {
+    camposObrigatorios.forEach((campo, indexCampo) => {
+        validarCampos(campo, indexCampo);
+        if (campo.classList.contains("invalido")) {
+            btnEnviar.setAttribute("type", "button");
+        } else if(campo.classList.contains("valido")) {
+            btnEnviar.setAttribute("type", "submit");
+        }
+    });
+})
 
 camposObrigatorios.forEach((campo, indexCampo) => {
     campo.addEventListener("blur", () => {
-        if (campo.value == "") {
-            campoInvalido(campo);
-            mensagemErro.forEach((erro, indexErro) => {
-                if (indexErro == indexCampo) {
-                    erro.classList.add("ativo");
-                }
-            });
-        } else {
-            campoValido(campo);
-            mensagemErro.forEach((erro, indexErro) => {
-                if (indexErro == indexCampo) {
-                    erro.classList.remove("ativo");
-                }
-            });
-        }
+        validarCampos(campo, indexCampo);
     });
 });
+
+function validarCampos(campo, indexCampo) {
+    if (campo.value.trim() == "") {
+        campoInvalido(campo);
+        mensagemErro.forEach((erro, indexErro) => {
+            if (indexErro == indexCampo) {
+                erro.classList.add("ativo");
+            }
+        });
+    } else {
+        campoValido(campo);
+        mensagemErro.forEach((erro, indexErro) => {
+            if (indexErro == indexCampo) {
+                erro.classList.remove("ativo");
+            }
+        });
+    }
+}
 
 function campoInvalido(campo) {
     campo.classList.remove("valido");
