299f7f206abee3239f727116b9e35f97cdd4daa0
Yasmim Coimbra <ymdsc.coimbra85@gmail.com>
2025-02-12 21:20:39 -0300

    JS: adding functionality: validating any input different than "" and vice-versa

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
 camposObrigatorios = document.querySelectorAll(".campo-obrigatorio");
 mensagemErro = document.querySelectorAll(".erro");
+
+camposObrigatorios.forEach((campo, indexCampo) => {
+    campo.addEventListener("blur", () => {
+        if (campo.value == "") {
+            campoInvalido(campo);
+            mensagemErro.forEach((erro, indexErro) => {
+                if (indexErro == indexCampo) {
+                    erro.classList.add("ativo");
+                }
+            });
+        } else {
+            campoValido(campo);
+            mensagemErro.forEach((erro, indexErro) => {
+                if (indexErro == indexCampo) {
+                    erro.classList.remove("ativo");
+                }
+            });
+        }
+    });
+});
